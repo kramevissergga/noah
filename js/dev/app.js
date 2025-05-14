18f9bbe36a41a4f8d63f4898e6c811f27379c3f2
kramevissergga <135664328+kramevissergga@users.noreply.github.com>
2025-05-14 22:49:22 +0300

    Add files via upload

--- js/dev/app.js
+++ js/dev/app.js
@@ -3836,6 +3836,38 @@ document.addEventListener("DOMContentLoaded", function() {
     feedbackSlider.mount();
   });
 });
+const scrollers$3 = document.querySelectorAll(".scroller");
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation$3();
+}
+function addAnimation$3() {
+  scrollers$3.forEach((scroller) => {
+    scroller.setAttribute("data-animated", true);
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
+const scrollers$2 = document.querySelectorAll(".scroller");
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation$2();
+}
+function addAnimation$2() {
+  scrollers$2.forEach((scroller) => {
+    scroller.setAttribute("data-animated", true);
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
 const scrollers$1 = document.querySelectorAll(".scroller");
 if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
   addAnimation$1();
